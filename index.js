69d384b92da04072bf39fd43e0203711780abcfe
Gloria Guy <totallygloria@gmail.com>
2016-10-23 15:05:18 -0700

    Added route handling by doing dependency injection in the index.js script (instead of hard-coding it into the router.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var server = require('./server');
 var router = require('./router');
 var url = require('url');
+var requestHandlers = require('./requestHandlers');
 
 
-server.start(router.route);
+var handler = {};
+handle['/'] = requestHandlers.start;
+handle['start'] = requestHandlers.start;
+handle['upload'] = requestHandlers.upload;
+
+server.start(router.route, handle);
