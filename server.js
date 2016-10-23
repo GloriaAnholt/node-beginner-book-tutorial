b0e8a15e0f05d73e9015cb171596bbd3659a7986
Gloria Guy <totallygloria@gmail.com>
2016-10-23 16:36:28 -0700

    Added async function to collect POST data into chunks and then call the router once all the chunks had been assembled.

--- server.js
+++ server.js
@@ -6,10 +6,21 @@ const url = require('url');     // help extract the url from the request
 
 function start(route, handle) {
   function onRequest(request, response) { // request details not important atm
+    var postData = '';
     var pathname = url.parse(request.url).pathname;
     console.log('Request for ' + pathname + ' received.');
 
-    route(handle, pathname, response);
+    request.setEncoding('utf8');
+
+    request.addListener('data', function(postDataChunk) {
+      postData += postDataChunk;
+      console.log('Received POST data chunk ' + postDataChunk + '.');
+    });
+
+    request.addListener('end', function() {
+      route(handle, pathname, response, postData);
+      }
+    );
   }
 
   http.createServer(onRequest).listen(8888);  // indicates which port to listen on
