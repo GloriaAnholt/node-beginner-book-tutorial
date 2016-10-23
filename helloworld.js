4e36a32fcc400b50e74bcf92143a1c0a4b7f4bfc
Gloria Guy <totallygloria@gmail.com>
2016-10-23 13:49:23 -0700

    Outline of the project goals and TODOs for getting started with a node web server.

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,15 @@
+// Project goal:
+// Open a landing page at /start
+// Serve an image at /upload
+
+// To Do
+// HTTP server to serve web pages
+// router to handle urls
+// request handlers
+// manage POST data with request data handlers
+// view logic for handlers to send things to broswer
+// upload handler to deal with images
+
+
+
 console.log('helllllo world');
