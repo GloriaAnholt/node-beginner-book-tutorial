ae6b3093c33eec1e1e953f0bf66a2befc7e02d33
Gloria Guy <totallygloria@gmail.com>
2016-10-23 14:19:20 -0700

    Checking the first to do off our list, we now have a working http server.

--- helloworld.js
+++ helloworld.js
@@ -2,13 +2,13 @@
 // Open a landing page at /start
 // Serve an image at /upload
 
-// To Do
-// HTTP server to serve web pages
-// router to handle urls
-// request handlers
-// manage POST data with request data handlers
-// view logic for handlers to send things to broswer
-// upload handler to deal with images
+// To Do's
+// DONE: HTTP server to serve web pages
+// TODO: router to handle urls
+// TODO: request handlers
+// TODO: manage POST data with request data handlers
+// TODO: view logic for handlers to send things to broswer
+// TODO: upload handler to deal with images
 
 
 
