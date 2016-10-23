c4b805ce4e7360fd5f545aa9a2baccd7ab5ddbc1
Gloria Guy <totallygloria@gmail.com>
2016-10-23 14:16:45 -0700

    Turned the server into a module so it can be exported/required elsewhere.

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
+// Convert the server into a module and export it
+
 var http = require('http');
 
-function onRequest(request, response) { // request details not important atm
-  console.log('Request received');
-  response.writeHead(200, {'Content-type': 'text/plain'});  // creates the header
-  response.write('Hello world');    // creates the response body
-  response.end();   // finishes the response
+function start() {
+  function onRequest(request, response) { // request details not important atm
+    console.log('Request received');
+    response.writeHead(200, {'Content-type': 'text/plain'});  // creates the header
+    response.write('Hello world');    // creates the response body
+    response.end();   // finishes the response
+  }
+
+  http.createServer(onRequest).listen(8888);  // indicates which port to listen on
+
+  console.log('Server has started');
 }
 
-http.createServer(onRequest).listen(8888);  // indicates which port to listen on
-
-console.log('Server has started');
+exports.start = start;
